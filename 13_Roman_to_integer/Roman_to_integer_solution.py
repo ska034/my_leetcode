d04524e9524e83afbc908751aeffa2ed9a2bb68f
ska034 <al.ig.erofeev@mail.ru>
2022-09-10 20:58:03 +0300

    Added task #13

--- 13_Roman_to_integer/Roman_to_integer_solution.py
+++ 13_Roman_to_integer/Roman_to_integer_solution.py
@@ -1,30 +1,26 @@
 # https://leetcode.com/problems/roman-to-integer/
 
 class Solution(object):
-    def intToRoman(self, num):
+    def romanToInt(self, s):
         """
-        :type num: int
-        :rtype: str
+        :type s: str
+        :rtype: int
         """
         dict = {
-            1: "I",
-            5: "V",
-            10: "X",
-            50: "L",
-            100: "C",
-            500: "D",
-            1000: "M"
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
         }
-        res = []
-
-
-        # if num // 1 > 0:
-        #     if num // 1 == 4:
-        #         res.append("IV")
-        #     else:
-        #         res.append(num // 1 * "I")
+        prev_val, res = 0, 0
+        for val in list(s):
+            if prev_val != 0 and prev_val < dict[val]:
+                res = res + dict[val] - 2 * prev_val
+            else:
+                res =res + dict[val]
+            prev_val = dict[val]
 
         return res
-
-
-print(Solution().intToRoman(19))
